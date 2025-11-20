467ac28b80cecb42a165ec4b38b1a09c6ce69b69
Apples1243 <Apples1243@naver.com>
2025-11-20 22:16:18 +0900

    [Feat] DB 커넥션 및 DB 업데이트

--- database/ddl/table.sql
+++ database/ddl/table.sql
@@ -1,96 +1,74 @@
--- MySQL Workbench Forward Engineering
-
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
--- -----------------------------------------------------
--- Schema management
--- -----------------------------------------------------
 
--- -----------------------------------------------------
--- Schema management
--- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `management` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ;
 USE `management` ;
 
--- -----------------------------------------------------
--- Table `management`.`certificate`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `management`.`certificate` (
-  `cert_id` INT NOT NULL,
+DROP TABLE IF EXISTS member_certificate;
+DROP TABLE IF EXISTS certificate;
+DROP TABLE IF EXISTS member;
+DROP TABLE IF EXISTS department;
+
+CREATE TABLE IF NOT EXISTS department (
+  `dept_id` int NOT NULL,
+  `dept_name` varchar(50) NOT NULL,
+  PRIMARY KEY (`dept_id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+CREATE TABLE IF NOT EXISTS certificate (
+  `cert_id` INT AUTO_INCREMENT NOT NULL,
   `cert_name` VARCHAR(45) NOT NULL,
-  `agency` VARCHAR(45) NOT NULL,
+  `cert_job` text,
+  `cert_summary` text,
+  `cert_trend` text,
   PRIMARY KEY (`cert_id`),
-  UNIQUE INDEX `cert_name_UNIQUE` (`cert_name` ASC) VISIBLE)
+  UNIQUE KEY `cert_name_UNIQUE` (`cert_name` ASC))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
-
--- -----------------------------------------------------
--- Table `management`.`department`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `management`.`department` (
-  `dept_id` TEXT NULL DEFAULT NULL,
-  `dept_name` TEXT NULL DEFAULT NULL,
-  `school_id` TEXT NULL DEFAULT NULL,
-  `series_code` BIGINT NULL DEFAULT NULL)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table `management`.`member`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `management`.`member` (
+CREATE TABLE IF NOT EXISTS member (
   `member_id` VARCHAR(45) NOT NULL,
+  `member_password` VARCHAR(45) NOT NULL,  
   `member_name` VARCHAR(45) NOT NULL,
   `member_email` VARCHAR(45) NOT NULL,
-  `member_password` VARCHAR(45) NOT NULL,
-  `dept_id` INT NULL DEFAULT NULL,
+  `is_admin` boolean NOT NULL DEFAULT FALSE,
+  `dept_id` INT NOT NULL,
   PRIMARY KEY (`member_id`),
-  INDEX `fk_member_department1_idx` (`dept_id` ASC) VISIBLE)
+  INDEX `fk_member_department1_idx` (`dept_id` ASC),
+  CONSTRAINT `fk_member_department1`
+    FOREIGN KEY (`dept_id`)
+    REFERENCES department (`dept_id`) 
+    ON DELETE NO ACTION 
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
-
--- -----------------------------------------------------
--- Table `management`.`member_certificate`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `management`.`member_certificate` (
+CREATE TABLE IF NOT EXISTS member_certificate (
+  `member_cert_id` int AUTO_INCREMENT NOT NULL,
   `member_id` VARCHAR(45) NOT NULL,
   `cert_id` INT NOT NULL,
-  `acquisition_date` DATE NOT NULL,
-  `grade` VARCHAR(45) NULL DEFAULT NULL,
-  PRIMARY KEY (`member_id`, `cert_id`),
-  INDEX `fk_member_has_certificate_certificate1_idx` (`cert_id` ASC) VISIBLE,
-  INDEX `fk_member_has_certificate_member1_idx` (`member_id` ASC) VISIBLE,
+  PRIMARY KEY (`member_cert_id`),
+  INDEX `fk_member_has_certificate_certificate1_idx` (`cert_id` ASC),
+  INDEX `fk_member_has_certificate_member1_idx` (`member_id` ASC),
   CONSTRAINT `fk_member_has_certificate_certificate1`
     FOREIGN KEY (`cert_id`)
-    REFERENCES `management`.`certificate` (`cert_id`))
+    REFERENCES `management`.`certificate` (`cert_id`)
+    ON DELETE CASCADE 
+    ON UPDATE CASCADE,
+  CONSTRAINT `fk_member_has_certificate_member1`
+    FOREIGN KEY (`member_id`)
+    REFERENCES `management`.`member` (`member_id`)
+    ON DELETE CASCADE 
+    ON UPDATE CASCADE)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table `management`.`school`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `management`.`school` (
-  `school_id` INT NOT NULL,
-  `school_name` VARCHAR(50) NOT NULL,
-  `homepage` VARCHAR(255) NULL DEFAULT NULL,
-  PRIMARY KEY (`school_id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
