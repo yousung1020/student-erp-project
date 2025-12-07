cca3ac18013af039ec136792d1610493c5f81adf
HwangSangIm <apples1243@dongyang.ac.kr>
2025-12-07 18:54:17 +0900

    Update AdminCertificateController.java

--- src/main/java/controller/admin/AdminCertificateController.java
+++ src/main/java/controller/admin/AdminCertificateController.java
@@ -22,7 +22,6 @@ public class AdminCertificateController extends HttpServlet{
 	}
     
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {   
-        request.setCharacterEncoding("UTF-8");
 		String action = request.getParameter("action");		
 		if ("create".equals(action)) {
             handleCreateRequest(request, response);
